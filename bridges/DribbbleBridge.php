fe5a4561ae43f96902d8a352c24b509f3fec30f7
Thomas <bikertom@gmx.at>
2020-11-29 13:43:42 +0100

    [DribbbleBridge] Fixed "img" tag markup.
    Previously the whole "srcset" string was added to the "src" attribute which caused images not to display. The "src" attribute now only contains a single URL and a separate "srcset" attribute was added to the "img" tag.

--- bridges/DribbbleBridge.php
+++ bridges/DribbbleBridge.php
@@ -86,7 +86,8 @@ favicon-63b2904a073c89b52b19aa08cebc16a154bcf83fee8ecc6439968b1e6db569c7.ico';
 
 	private function getImageTag($preview_path, $title){
 		return sprintf(
-			'<br /> <a href="%s"><img src="%s" alt="%s" /></a>',
+			'<br /> <a href="%s"><img src="%s" srcset="%s" alt="%s" /></a>',
+			$this->getFullSizeImagePath($preview_path),
 			$this->getFullSizeImagePath($preview_path),
 			$preview_path,
 			$title
